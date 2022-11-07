8be2ab50081deb958562da5ab17af3e814f1bc99
Your Name01~ <fateme.ebrahimzadeh20@gmail.com>
2022-11-07 12:45:52 +0330

    add basic change for apllying theme change

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC, useState } from 'react';
+import { ThemeContext } from './contexts/theme-context';
+import Layout from './layout';
+import moon from './images/moon.png';
+import sun from './images/sun.png';
 
-function App() {
+import './App.scss';
+
+const App: FC = () => {
+  // Detecting the default theme
+  const isBrowserDefaulDark = () => window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+  const getDefaultTheme = (): string => {
+    const localStorageTheme = localStorage.getItem('default-theme');
+    const browserDefault = isBrowserDefaulDark() ? 'dark' : 'light';
+    return localStorageTheme || browserDefault;
+  };
+
+  const [theme, setTheme] = useState(getDefaultTheme());
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div className={`theme-${theme}`}>
+        <Layout>
+          <div className="content-wrapper">
+            <img src={theme === 'dark' ? moon : sun} alt={theme === 'dark' ? 'moon' : 'sun'} />
+          </div>
+        </Layout>
+      </div>
+    </ThemeContext.Provider>
   );
-}
+};
 
 export default App;
